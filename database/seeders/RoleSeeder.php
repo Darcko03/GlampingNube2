546b6069fa433e7eef46eaaf08d040ac41bde611
Darcko03 <138030432+Darcko03@users.noreply.github.com>
2023-09-20 20:35:53 -0500

    Update RoleSeeder.php

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,8 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\characteristic;
 use App\Models\Customer;
-use App\Models\Dome;
-use App\Models\Offer;
+use App\Models\dome;
+use App\Models\offer;
 use App\Models\PaymentMethod;
 use App\Models\Service;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -41,11 +41,11 @@ class RoleSeeder extends Seeder
         User::create(['name' => 'Jefe', 'email' => 'admin@nube.com', 'password' => '12345678']);
         User::create(['name' => 'Empleado', 'email' => 'asistente@nube.com', 'password' => '12345678']);
         characteristic::create(['name' => 'Tv Hd','description' => 'television mamalona']);
-        Dome::create(['name' => 'Margarita','state' => '0','price' => '40000','location' => 'sur','description' => 'Lindo domo en un lago','capacity' => '4']);
+        dome::create(['name' => 'Margarita','state' => '0','price' => '40000','location' => 'sur','description' => 'Lindo domo en un lago','capacity' => '4']);
         Service::create(['name' => 'Piscina','state' => '0','price' => '20000','quantity' => '10']);
         Service::create(['name' => 'cabalgata','state' => '0','price' => '30000','quantity' => '10']);
-        Offer::create(['name' => 'no aplica','discount' => '0']);
-        Offer::create(['name' => 'Cumpleaños','discount' => '10000']);
+        offer::create(['name' => 'no aplica','discount' => '0']);
+        offer::create(['name' => 'Cumpleaños','discount' => '10000']);
         Customer::create(['name' => 'pablo','last_name' => 'perez','email' => 'pablito@gmail.com','phone_number' => '3024542233','birthdate' => '2023-09-07','city' => 'Medellin','address' => 'kra 17i#5a10']);
         PaymentMethod::create(['name' => 'visa']);
         
